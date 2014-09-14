ca736bd2e4a79123d7e798c169751ed2914fd701
Cory Forsyth <cory.forsyth@gmail.com>
2014-09-14 10:40:54 -0400

    update to latest ember cli

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
+import Ember from 'ember';
+
 var Router = Ember.Router.extend({
-  location: ENV.locationType
+  location: SendmashnoteENV.locationType
 });
 
 Router.map(function() {
   this.route('about');
   this.route('team');
   this.route('privacy');
-  this.route('message', {path: '/message/:message_id'});
-  this.route('message_sent');
 });
 
 export default Router;
